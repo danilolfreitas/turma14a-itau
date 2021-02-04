8432e8ba70a3a4f22ce5b22e9397e3864714b487
danilolfreitas <danilolfreitas@gmail.com>
2021-02-04 14:30:51 -0300

    terceiro teste do quadrado, arrumando as tags

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
+<head>
+
+<title>Pai Mesmo - Virtual Store</title>
+</head>
+<body>
+
 <h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 
 <h3> Um deploy aotomático feito com alunos do itaú </h3>
 
-<h5>Tentar sempre, desistir jamais!!!<h5>
+<h5>Tentar sempre, desistir jamais!!!</h5>
   
-<h7>testando!!!<h7>
-<h10>Tentar sempre, desistir as vezes!!!<h10>
-<h7>QUERO DELETAR TUDO </h7>
+<h6>testando!!!</h6>
+<h5>Tentar sempre, desistir as vezes!!!</h5>
+<h5>QUERO DELETAR TUDO </h5>
 
 <div style="width:200;height:200;background:yellow">
 
-    Um teste em um quadrado
+<center>Um teste em um quadrado</center>
 
 </div>
+
+</body>
