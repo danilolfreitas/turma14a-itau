de38204dd80f45bda837c3b1924a93856d4bdced
danilolfreitas <danilolfreitas@gmail.com>
2021-02-09 09:55:02 -0300

    nova Página em Construção

---
+++ indexExercicio.php
@@ -0,0 +1,37 @@
+<html>
+  <head>
+  	<title>Pai Mesmo - Virtual Store</title>
+  </head>
+  <body>
+    <h1>Bem vindo turma 14A do itaú pelo treinamento da Gama </h1>
+
+    <h3> Um deploy aotomático feito com alunos do itaú </h3>
+
+	<div style="display:flex">
+      <div style="width:200;height:400;background:yellow;padding:10;margin:10">
+          <center>Um teste em um quadrado
+          <img src="c1.jpg" width="150">
+
+            <br>
+            R$ 50,00
+            <br>
+            <a href="" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px" target="_blank">Comprar</a><br>
+            <a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+
+          </center>
+          
+      </div>
+
+		<div style="width:200;height:400;background:blue;color:white;padding:10;margin:10">
+          <center>Um teste em um quadrado
+          <img src="c2.jpg" width="150">
+           <br>
+            R$ 80,00
+            <br>
+            <a href="" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px" target="_blank">Comprar</a><br>
+            <a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
+          </center>
+      </div>
+	</div>
+  </body>
+</html>
